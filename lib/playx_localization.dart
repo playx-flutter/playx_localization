99c8fa33dc6a5c9bf74c977815ac4d02b11d6425
basemosama <engbasemosama@gmail.com>
2025-07-01 14:50:16 +0300

    chore: Update CHANGELOG for version 0.3.0 with enhancements and new extensions

--- lib/playx_localization.dart
+++ lib/playx_localization.dart
@@ -13,5 +13,6 @@ export 'package:playx_localization/src/playx_localization.dart';
 export 'package:playx_localization/src/widgets/playx_localization_builder.dart';
 export 'package:playx_localization/src/widgets/playx_inherited_localization.dart';
 export 'package:playx_localization/src/extensions/locale_extensions.dart';
+export 'package:playx_localization/src/easy_localization/asset_loader.dart';
 
 
